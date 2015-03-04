e0092d63d720d7c81800a69d2618a442675aa45d
seanachaidh <pvankeymeulen@seanachaidh.be>
2015-03-04 11:43:25 +0100

    Een verbindingstest maken

--- tests/databases.pas
+++ tests/databases.pas
@@ -9,23 +9,61 @@ uses
 
 type
 
+  { TDatabaseCase }
+
   TDatabaseCase= class(TTestCase)
   protected
     procedure SetUp; override;
+    procedure TearDown; override;
   published
     procedure TestHookUp;
+    procedure TestConnection;
+  end;
+
+  { TCommentsCase }
+
+  TCommentsCase = class(TTestCase)
+    protected
+      procedure SetUp; override;
+      procedure TearDown; override;
   end;
 
 implementation
+
+{ TCommentsCase }
+
+procedure TCommentsCase.SetUp;
+begin
+  inherited SetUp;
+end;
+
+procedure TCommentsCase.TearDown;
+begin
+  inherited TearDown;
+end;
 
 procedure TDatabaseCase.TestHookUp;
 begin
   Fail('Write your own test');
 end;
 
+procedure TDatabaseCase.TestConnection;
+var
+  testconnection: TSeanblogConnection;
+begin
+  testconnection:= TSeanblogConnection.Create;
+  testconnection.connect
+  AssertTrue(testconnection.isConnected);
+end;
+
 procedure TDatabaseCase.SetUp;
 begin
 
+end;
+
+procedure TDatabaseCase.TearDown;
+begin
+  inherited TearDown;
 end;
 
 
